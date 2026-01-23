00f6eb8d6f0ff9596ccc564c9676264dcace11aa
Alan Boulay <boulay.alan@gmail.com>
2026-01-23 12:50:58 +0700

    Switch the disk pattern for particles into a light pattern.

--- src/Shaders/galaxy/fragment.glsl
+++ src/Shaders/galaxy/fragment.glsl
@@ -1,9 +1,25 @@
 void main()
 {
+    // DISC PATTERN
+    /* float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = step(0.5, strength);
+    strength = 1.0 - strength; */
+
+    // DIFFUSE POINT PATTERN
+    // Calculate the distance to the center (0.0 at the center, 0.5 at the edges)
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+
+    // Multiply by 2 to get 1.0 at the center and 0.0 at the edge.    
+        // strength *= 2.0;
+    // Invert the value to obtain soft edges.
+        // strength = 1.0 - strength;
+
+
+    // LIGHT POINT PATTERN
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength = abs(1.0 - step(0.5, strength)); // Version nette
-    // For a milder version, use :
-    // strength = 1.0 - smoothstep(0.45, 0.5, strength);
+    strength = 1.0 - strength;
+    // Applying power to make the edge transitions more "organic"
+    strength = pow(strength, 7.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
