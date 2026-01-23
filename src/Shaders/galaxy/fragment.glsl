71c3b1d76abb3befa27fe97d526a16558e573bbc
Alan Boulay <boulay.alan@gmail.com>
2026-01-23 12:25:43 +0700

    Draw a disk pattern for particles

--- src/Shaders/galaxy/fragment.glsl
+++ src/Shaders/galaxy/fragment.glsl
@@ -1,7 +1,9 @@
-
 void main()
 {
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    float strength = distance(gl_PointCoord, vec2(0.5));
+    strength = abs(1.0 - step(0.5, strength)); // Version nette
+    // For a milder version, use :
+    // strength = 1.0 - smoothstep(0.45, 0.5, strength);
 
-    #include <colorspace_fragment>
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
